7f208c877085f5c9fe9d43964b1b5641319e36a7
gbzim <gbzim@local>
2026-06-05 19:10:50 -0300

    fix: add freeze_support to mkpfs_runner to fix multiprocessing in frozen exe

--- mkpfs_runner.py
+++ mkpfs_runner.py
@@ -1,3 +1,7 @@
-from mkpfs.cli import main
+import multiprocessing
 import sys
-sys.exit(main())
+
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
+    from mkpfs.cli import main
+    sys.exit(main())
